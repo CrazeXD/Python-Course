8c4800ffb2eb4a5ad2077459700091fb5cfcd5b9
craze <crayzmayz100@gmail.com>
2021-11-23 21:17:56 -0800

    11/23/21 Update 4

---
+++ input.py
@@ -0,0 +1,20 @@
+#To get user input, we can use the input() function in a variable
+
+variable = input("What is your favorite song?")
+
+#The text in the input function is the text you want on the line of your input. 
+#If you don't want any, you can leave it blank
+#The default input will take a string as input
+#To change that, you can add another function around it
+
+numinput = int(input("How old are you?"))
+print(numinput, 'years old')
+#There are other types of number values than int, but those can be ignored
+
+boolinput = bool(input("True or False?"))
+if boolinput: #Checks if true by default
+    print("Yes")
+elif boolinput == False:
+    print("No")
+
+#You can use any other type of value in the same way, just make sure you use the correct keyword
---
+++ stringfeatures.py
@@ -0,0 +1,34 @@
+# Strings are values that contain text
+# They can be placed either in a variable or as an argument in a function
+
+# Strings can have any charchter in them, however some require an extra step
+
+#The following features can be used in strings to make them more versatile
+
+newline = '\n'
+quotes = '\' or \"'
+print(newline, quotes)
+#To ignore the \ charachter as a key, use a double \
+
+noslash = "C:\\users\\"
+print(noslash)
+# Triple quotes can also be used in variables to add multiple lines
+
+multiline = '''
+Hi
+Hello
+Welcome
+Hola
+'''
+print(multiline)
+#If you want to put a variable in a string, you can do one of the following methods
+
+#A f string can be used, and you put the variable in the {} brackets
+
+name = "John"
+compname = "Alexa"
+print(f"Hello {name}, my name is {compname}.")
+
+#Another option is a .format()
+#Write the variables in order they appear in the string
+print("Hello {}, my name is {}.".format(name, compname))
