34a27d850b3ed3cb8c38adfbbe05c895003d3d2a
Craze <crayzmayz100@gmail.com>
2022-06-07 23:13:53 -0700

    Thin

---
+++ Intermediete/Class 4/tk1.py
@@ -0,0 +1,12 @@
+#Tkinter is an inbuilt module meant to help with 
+
+#For the next 3 classes, we will be working with the tkinter module
+from tkinter import *
+
+#To get a window, we need to call Tk()
+
+tk = Tk()
+
+tk.mainloop()
+
+#That will create a basic window
