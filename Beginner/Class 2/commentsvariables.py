e2d6e4dd412e4222d950b35a618796036ae4b2a0
Craze <crazexd@github.com>
2022-06-10 23:40:55 -0700

    ..

--- Beginner/Class 2/commentsvariables.py
+++ Beginner/Class 2/commentsvariables.py
@@ -1,13 +1,4 @@
 #use the # symbol to make comments that are not code
-
-#variables
-a = 'a'
-string = "Hello! I am a string"
-number = 1
-trueboolean = True
-falseboolean = False
-#Variables must not start with a number, and can't contain any special charachters(?, !, #, $) except the _
-
 #Use triple quotes to add multiple lines of comments
 
 '''
@@ -17,6 +8,16 @@ Line 3
 Line 4
 '''
 
+#variables
+a = 'a' #Char
+string = "Hello! I am a string" #String
+number = 1 #Integer
+floater = 1.6 #Float
+trueboolean = True
+falseboolean = False
+print(falseboolean)
+#Variables must not start with a number, and can't contain any special charachters(?, !, #, $) except the _
+
 #To print a variable, do not use quotations
 
 print(string)
@@ -25,3 +26,7 @@ print(a)
 #In order to have multiple variables or types of content in the same print statement, seperate it with a comma
 
 print(a, number, "Hello world!")
+
+#We can make variables equal to each other
+falseboolean = trueboolean
+print(falseboolean)
